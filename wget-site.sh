d22a273a8f10fa2ba3586f768f47fe0a7abe4cfd
Marcel Araujo <marcel.araujo@inphonex.com>
2013-02-07 10:42:23 -0200

    Adding script

---
+++ wget-site.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$#" != 1 ]; then 
+	echo 'Usage wget-site.sh http://www.site.com'
+	exit 1
+fi
+
+SITE="$1"
+DOMAIN=$(echo "$SITE" | egrep -o '(([a-zA-Z0-9]([a-zA-Z0-9\-]{0,65}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6})')
+
+wget \
+	--random-wait \
+	--recursive \
+	--page-requisites \
+	--user-agent="Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)" \
+	--mirror \
+	-erobots=off \
+	--convert-links \
+	--domains $DOMAIN $SITE
